4a89ee027dbfa6aee1334cc277eb0af51c9e0932
zysite <zysite@outlook.com>
2019-03-09 11:50:54 +0800

    Add additional params

--- parser/commands/train.py
+++ parser/commands/train.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-from parser import BiAffineParser, Model
+from parser import BiaffineParser, Model
 from parser.utils import Corpus, Embedding, TextDataset, Vocab, collate_fn
 
 import torch
@@ -24,27 +23,11 @@ class Train(object):
                                help='path to test file')
         subparser.add_argument('--fembed', default='data/glove.6B.100d.txt',
                                help='path to pretrained embedding file')
-        subparser.add_argument('--file', '-f', default='model.pt',
-                               help='path to model file')
-        subparser.add_argument('--seed', '-s', default=1, type=int,
-                               help='seed for generating random numbers')
-        subparser.add_argument('--threads', '-t', default=4, type=int,
-                               help='max num of threads')
-        subparser.add_argument('--device', '-d', default='-1',
-                               help='ID of GPU to use')
         subparser.set_defaults(func=self)
 
         return subparser
 
     def __call__(self, args):
-        print(f"Set the max num of threads to {args.threads}")
-        print(f"Set the seed for generating random numbers to {args.seed}")
-        torch.set_num_threads(args.threads)
-        torch.manual_seed(args.seed)
-
-        print(f"Set the device with ID {args.device} visible")
-        os.environ['CUDA_VISIBLE_DEVICES'] = args.device
-
         print("Preprocess the data")
         train = Corpus.load(args.ftrain)
         dev = Corpus.load(args.fdev)
@@ -52,6 +35,7 @@ class Train(object):
         embed = Embedding.load(args.fembed)
         vocab = Vocab.from_corpus(corpus=train, min_freq=2)
         vocab.read_embeddings(embed=embed, unk='unk')
+        torch.save(vocab, args.vocab)
         print(vocab)
 
         print("Load the dataset")
@@ -75,25 +59,29 @@ class Train(object):
 
         print("Create the model")
         params = {
+            'n_words': vocab.n_train_words,
             'n_embed': Config.n_embed,
-            'n_char_embed': Config.n_char_embed,
-            'n_char_out': Config.n_char_out,
+            'n_tags': vocab.n_tags,
+            'n_tag_embed': Config.n_tag_embed,
             'embed_dropout': Config.embed_dropout,
             'n_lstm_hidden': Config.n_lstm_hidden,
             'n_lstm_layers': Config.n_lstm_layers,
             'lstm_dropout': Config.lstm_dropout,
             'n_mlp_arc': Config.n_mlp_arc,
-            'n_mlp_lab': Config.n_mlp_lab,
-            'mlp_dropout': Config.mlp_dropout
+            'n_mlp_rel': Config.n_mlp_rel,
+            'mlp_dropout': Config.mlp_dropout,
+            'n_rels': vocab.n_rels,
+            'pad_index': vocab.pad_index,
+            'unk_index': vocab.unk_index
         }
         for k, v in params.items():
             print(f"  {k}: {v}")
-        parser = BiAffineParser(vocab, params)
+        network = BiaffineParser(params, vocab.embeddings)
         if torch.cuda.is_available():
-            parser = parser.cuda()
-        print(f"{parser}\n")
+            network = network.cuda()
+        print(f"{network}\n")
 
-        model = Model(parser=parser)
+        model = Model(vocab, network)
         model(loaders=(train_loader, dev_loader, test_loader),
               epochs=Config.epochs,
               patience=Config.patience,
