28f75dfb5fff37f74c12b687d1bf524aedccd0cd
zysite <zysite@outlook.com>
2019-03-09 11:48:42 +0800

    Replace char_lstm with tag embedding

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from parser.modules import CHAR_LSTM, LSTM, MLP, Biaffine
+from parser.modules import LSTM, MLP, Biaffine
 from parser.modules.dropout import IndependentDropout, SharedDropout
 
 import torch
@@ -9,24 +9,22 @@ from torch.nn.utils.rnn import (pack_padded_sequence, pad_packed_sequence,
                                 pad_sequence)
 
 
-class BiAffineParser(nn.Module):
+class BiaffineParser(nn.Module):
 
-    def __init__(self, vocab, params):
-        super(BiAffineParser, self).__init__()
+    def __init__(self, params, embeddings):
+        super(BiaffineParser, self).__init__()
 
-        self.vocab = vocab
         self.params = params
         # the embedding layer
-        self.embed = nn.Embedding(vocab.n_train_words, params['n_embed'])
-        self.pretrained = nn.Embedding.from_pretrained(vocab.embeddings)
-        # the char-lstm layer
-        self.char_lstm = CHAR_LSTM(n_char=vocab.n_chars,
-                                   n_embed=params['n_char_embed'],
-                                   n_out=params['n_char_out'])
+        self.pretrained = nn.Embedding.from_pretrained(embeddings)
+        self.embed = nn.Embedding(num_embeddings=params['n_words'],
+                                  embedding_dim=params['n_embed'])
+        self.tag_embed = nn.Embedding(num_embeddings=params['n_tags'],
+                                      embedding_dim=params['n_tag_embed'])
         self.embed_dropout = IndependentDropout(p=params['embed_dropout'])
 
         # the word-lstm layer
-        self.lstm = LSTM(input_size=params['n_embed']+params['n_char_out'],
+        self.lstm = LSTM(input_size=params['n_embed']+params['n_tag_embed'],
                          hidden_size=params['n_lstm_hidden'],
                          num_layers=params['n_lstm_layers'],
                          dropout=params['lstm_dropout'],
@@ -40,42 +38,43 @@ class BiAffineParser(nn.Module):
         self.mlp_arc_d = MLP(n_in=params['n_lstm_hidden']*2,
                              n_hidden=params['n_mlp_arc'],
                              dropout=params['mlp_dropout'])
-        self.mlp_lab_h = MLP(n_in=params['n_lstm_hidden']*2,
-                             n_hidden=params['n_mlp_lab'],
+        self.mlp_rel_h = MLP(n_in=params['n_lstm_hidden']*2,
+                             n_hidden=params['n_mlp_rel'],
                              dropout=params['mlp_dropout'])
-        self.mlp_lab_d = MLP(n_in=params['n_lstm_hidden']*2,
-                             n_hidden=params['n_mlp_lab'],
+        self.mlp_rel_d = MLP(n_in=params['n_lstm_hidden']*2,
+                             n_hidden=params['n_mlp_rel'],
                              dropout=params['mlp_dropout'])
 
         # the Biaffine layers
         self.arc_attn = Biaffine(n_in=params['n_mlp_arc'],
                                  bias_x=True,
                                  bias_y=False)
-        self.lab_attn = Biaffine(n_in=params['n_mlp_lab'],
-                                 n_out=vocab.n_labels,
+        self.rel_attn = Biaffine(n_in=params['n_mlp_rel'],
+                                 n_out=params['n_rels'],
                                  bias_x=True,
                                  bias_y=True)
+        self.pad_index = params['pad_index']
+        self.unk_index = params['unk_index']
 
         self.reset_parameters()
 
     def reset_parameters(self):
         nn.init.zeros_(self.embed.weight)
 
-    def forward(self, words, chars):
+    def forward(self, words, tags):
         # get the mask and lengths of given batch
-        mask = words.gt(0)
+        mask = words.ne(self.pad_index)
         lens = mask.sum(dim=1)
         # get outputs from embedding layers
-        word_embed = self.pretrained(words)
-        word_embed += self.embed(
-            words.masked_fill_(words.ge(self.vocab.n_train_words),
-                               self.vocab.unk_index)
+        embed = self.pretrained(words)
+        embed += self.embed(
+            words.masked_fill_(words.ge(self.embed.num_embeddings),
+                               self.unk_index)
         )
-        char_embed = self.char_lstm(chars[mask])
-        char_embed = pad_sequence(torch.split(char_embed, lens.tolist()), True)
-        word_embed, char_embed = self.embed_dropout(word_embed, char_embed)
-        # concatenate the word and char representations
-        x = torch.cat((word_embed, char_embed), dim=-1)
+        tag_embed = self.tag_embed(tags)
+        embed, tag_embed = self.embed_dropout(embed, tag_embed)
+        # concatenate the word and tag representations
+        x = torch.cat((embed, tag_embed), dim=-1)
 
         sorted_lens, indices = torch.sort(lens, descending=True)
         inverse_indices = indices.argsort()
@@ -87,33 +86,36 @@ class BiAffineParser(nn.Module):
         # apply MLPs to the LSTM output states
         arc_h = self.mlp_arc_h(x)
         arc_d = self.mlp_arc_d(x)
-        lab_h = self.mlp_lab_h(x)
-        lab_d = self.mlp_lab_d(x)
+        rel_h = self.mlp_rel_h(x)
+        rel_d = self.mlp_rel_d(x)
 
-        # get arc and label scores from the bilinear attention
+        # get arc and rel scores from the bilinear attention
         # [batch_size, seq_len, seq_len]
         s_arc = self.arc_attn(arc_d, arc_h)
-        # [batch_size, seq_len, seq_len, n_labels]
-        s_lab = self.lab_attn(lab_d, lab_h).permute(0, 2, 3, 1)
+        # [batch_size, seq_len, seq_len, n_rels]
+        s_rel = self.rel_attn(rel_d, rel_h).permute(0, 2, 3, 1)
         # set the scores that exceed the length of each sentence to -inf
         s_arc.masked_fill_((1 - mask).unsqueeze(1), float('-inf'))
 
-        return s_arc, s_lab
+        return s_arc, s_rel
 
     @classmethod
     def load(cls, fname):
-        state = torch.load(fname)
-        parser = cls(state['vocab'], state['params'])
-        parser.load_state_dict(state['state_dict'])
         if torch.cuda.is_available():
-            parser = parser.cuda()
+            device = torch.device('cuda')
+        else:
+            device = torch.device('cpu')
+        state = torch.load(fname, map_location=device)
+        network = cls(state['params'], state['embeddings'])
+        network.load_state_dict(state['state_dict'])
+        network.to(device)
 
-        return parser
+        return network
 
     def save(self, fname):
         state = {
-            'vocab': self.vocab,
             'params': self.params,
+            'embeddings': self.pretrained.weight,
             'state_dict': self.state_dict(),
         }
         torch.save(state, fname)
