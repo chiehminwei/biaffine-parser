fixed attention mask, it's not token_start_mask

parser/model.py
@@ -67,21 +67,21 @@ class Model(object):
     def train(self, loader):
         self.network.train()
         i = 0
-        for words, mask, arcs, rels in tqdm(loader):
+        for words, attention_mask, token_start_mask, arcs, rels in tqdm(loader):
             if i > 0: assert 1 == 2
             self.optimizer.zero_grad()
-            # mask = words.ne(self.vocab.pad_index)
-            # ignore the first token of each sentence (<ROOT>)
-            mask[:, 1] = 0
-            s_arc, s_rel = self.network(words, mask)
+            s_arc, s_rel = self.network(words, attention_mask)
+
             # ignore [CLS]
-            mask[:, 0] = 0
+            token_start_mask[:, 0] = 0
+            # ignore <ROOT>
+            token_start_mask[:, 1] = 0
             # ignore [SEP], don't need to subtract 1 from lens since <ROOT> is also 0
             lens = words.ne(self.vocab.pad_index).sum(dim=1)
-            mask[torch.arange(len(mask)), lens] = 0
+            token_start_mask[torch.arange(len(token_start_mask)), lens] = 0            
 
-            s_arc, s_rel = s_arc[mask], s_rel[mask]
-            gold_arcs, gold_rels = arcs[mask], rels[mask]
+            s_arc, s_rel = s_arc[token_start_mask], s_rel[token_start_mask]
+            gold_arcs, gold_rels = arcs[token_start_mask], rels[token_start_mask]
 
             loss = self.get_loss(s_arc, s_rel, gold_arcs, gold_rels)
             loss.backward()

parser/utils/vocab.py
@@ -94,11 +94,13 @@ class Vocab(object):
         arcs_numerical = []
         rels_numerical = []
         token_start_mask = []
+        attention_mask = []
         for words, arcs, rels in zip(corpus.words, corpus.heads, corpus.rels):
             sentence_token_ids = []
             sentence_arc_ids = []
             sentence_rel_ids = []
             token_starts = []
+            attentions = []
             words = ['[CLS]'] + words + ['[SEP]']
             arcs = [0] + arcs + [0]
             rels = ['PAD'] + rels + ['PAD']
@@ -113,11 +115,13 @@ class Vocab(object):
                 sentence_arc_ids.extend([arc] * len(tokens))
                 sentence_rel_ids.extend([self.rel_dict.get(rel, 0)] * len(tokens))
                 token_starts.extend([1] + [0] * (len(tokens) - 1))
+                attentions.extend([1] * len(tokens))
             words_numerical.append(torch.tensor(sentence_token_ids))
             arcs_numerical.append(torch.tensor(sentence_arc_ids))
             rels_numerical.append(torch.tensor(sentence_rel_ids))
             token_start_mask.append(torch.ByteTensor(token_starts))
-        return words_numerical, token_start_mask, arcs_numerical, rels_numerical
+            attention_mask.append(torch.ByteTensor(attentions))
+        return words_numerical, attention_mask, token_start_mask, arcs_numerical, rels_numerical
 
 
     def yeet(self, corpus):

run.py
@@ -9,7 +9,7 @@ import logging
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    # logging.basicConfig(level=logging.INFO)
     # torch.set_printoptions(threshold=10000)
 
     parser = argparse.ArgumentParser(