79f90bd981a2220350bc67dbe89836c5493ad4aa
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-22 04:36:39 +0000

    yeet

--- parser/model.py
+++ parser/model.py
@@ -73,7 +73,7 @@ class Model(object):
     def train(self, loader):
         self.network.train()
         
-        corpus = Corpus.load(args.fdata)
+        corpus = Corpus.load('data/train.conll')
         print("Predict the dataset")
         corpus.heads, corpus.rels = self.predict(loader)
 
