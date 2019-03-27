255954c073328c4cc024a9d4dd3b146ab11cfc2d
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-27 02:18:35 +0000

    skip training

--- parser/utils/reader.py
+++ parser/utils/reader.py
@@ -78,7 +78,7 @@ class Corpus(object):
             if len(line) <= 1:
                 sentence = Sentence(*zip(*[l.split() for l in lines[start:i] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]]))
                 if len([l.split() for l in lines[start:i] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]]) == 0:
-                    asset 1 == 2 
+                    assert 1 == 2 
                 sentences.append(sentence)
                 start = i + 1
         corpus = cls(sentences)
