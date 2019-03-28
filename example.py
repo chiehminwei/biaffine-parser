64767a9a734d9c78edd44f849d503f731b7049b9
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-28 14:04:44 +0000

    yeet

--- example.py
+++ example.py
@@ -7,15 +7,13 @@ import torch
 from torch.utils.data import DataLoader
 
 
-BATCH_SIZE = 32
-CHECKPOINT_DIR = 'model.pt'
-VOCAB_DIR = 'vocab.pt'
+BATCH_SIZE = 32				# only affects speed
+CHECKPOINT_DIR = 'model.pt' # you'll need to change this
+VOCAB_DIR = 'vocab.pt'		# and this
 
 vocab = torch.load(VOCAB_DIR)
 network = BiaffineParser.load(CHECKPOINT_DIR)
 model = Model(vocab, network)
-
-sentences = [['I', 'luv', 'Chloe', '.'], ['But', 'maybe', 'not', 'anymore', ':)']]
 
 def example(sentences):
 
@@ -25,32 +23,8 @@ def example(sentences):
 	                    collate_fn=collate_fn)
 
 	# Three options
-	print('embeddings')
 	embeddings = model.get_embeddings(loader)
-	print(len(embeddings))
-	print(len(embeddings[0]))
-	print(len(embeddings[1]))
-	print(len(embeddings[0][0]))
-
-	print('s_arc, s_rel')
 	s_arc, s_rel = model.get_matrices(loader)
-	print(len(s_arc), len(s_rel))
-	print(len(s_arc[0]), len(s_rel[0]))
-	print(len(s_arc[1]), len(s_rel[1]))
-	print(len(s_arc[0][0]), len(s_rel[0][0]))
-	print(len(s_arc[0][0]), len(s_rel[0][0][0]))
-
-	print('embeddings')
 	s_arc, s_rel, embeddings = model.get_everything(loader)
-	print(len(embeddings))
-	print(len(embeddings[0]))
-	print(len(embeddings[1]))
-	print(len(embeddings[0][0]))
-
-	print('s_arc, s_rel')
-	print(len(s_arc), len(s_rel))
-	print(len(s_arc[0]), len(s_rel[0]))
-	print(len(s_arc[1]), len(s_rel[1]))
-	print(len(s_arc[0][0]), len(s_rel[0][0]))
 
 example(sentences)
