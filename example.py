6657225023f2cc68e0b0bd1290122e05c873c639
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-28 20:50:45 +0000

    updated example to include basic bert

--- example.py
+++ example.py
@@ -2,17 +2,33 @@
 
 from parser import BiaffineParser, Model
 from parser.utils import TextDataset, collate_fn
-
 import torch
 from torch.utils.data import DataLoader
 
 
-BATCH_SIZE = 32				# only affects speed
+BATCH_SIZE = 32				# only affects speed, if too big you could OOM
 CHECKPOINT_DIR = 'model.pt' # you'll need to change this
 VOCAB_DIR = 'vocab.pt'		# and this
 
 vocab = torch.load(VOCAB_DIR)
-network = BiaffineParser.load(CHECKPOINT_DIR)
+
+bert = BertModel.from_pretrained('bert-base-multilingual-cased')
+
+# what's in the params won't affect embeddings, they're just here so that my initialization code doesn't break
+params = {
+	'n_words': vocab.n_train_words,
+	'n_chars': vocab.n_chars,
+	'word_dropout': Config.word_dropout,
+	'n_bert_hidden': Config.n_bert_hidden,
+	'bert_dropout': Config.bert_dropout,
+	'n_mlp_arc': Config.n_mlp_arc,
+	'n_mlp_rel': Config.n_mlp_rel,
+	'mlp_dropout': Config.mlp_dropout,
+	'n_rels': vocab.n_rels,
+	'pad_index': vocab.pad_index
+}
+network = BiaffineParser(params)			  # if you want to use the original (not tuned) BERT
+network = BiaffineParser.load(CHECKPOINT_DIR) # if you want to use the tuned BERT
 model = Model(vocab, network)
 
 sentences = [['Yes', 'yes', 'yes', '.'], ["It's", 'all', 'done', ':)']]
