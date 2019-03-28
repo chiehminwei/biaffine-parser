93dccba99c32a27f9d7e95251bda8361d6a61ff3
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-28 13:58:29 +0000

    yeet

--- example.py
+++ example.py
@@ -15,6 +15,7 @@ vocab = torch.load(VOCAB_DIR)
 network = BiaffineParser.load(CHECKPOINT_DIR)
 model = Model(vocab, network)
 
+sentences = [['I', 'luv', 'Chloe', '.'], ['But', 'maybe', 'not', 'anymore', ':)']]
 
 def example(sentences):
 
@@ -24,6 +25,31 @@ def example(sentences):
 	                    collate_fn=collate_fn)
 
 	# Three options
+	print('embeddings')
 	embeddings = model.get_embeddings(loader)
+	print(len(embeddings))
+	print(len(embeddings[0]))
+	print(len(embeddings[1]))
+	print(len(embeddings[0][0]))
+
+	print('s_arc, s_rel')
 	s_arc, s_rel = model.get_matrices(loader)
+	print(len(s_arc), len(s_rel))
+	print(len(s_arc[0]), len(s_rel[0]))
+	print(len(s_arc[1]), len(s_rel[1]))
+	print(len(s_arc[0][0]), len(s_rel[0][0]))
+
+	print('embeddings')
 	s_arc, s_rel, embeddings = model.get_everything(loader)
+	print(len(embeddings))
+	print(len(embeddings[0]))
+	print(len(embeddings[1]))
+	print(len(embeddings[0][0]))
+
+	print('s_arc, s_rel')
+	print(len(s_arc), len(s_rel))
+	print(len(s_arc[0]), len(s_rel[0]))
+	print(len(s_arc[1]), len(s_rel[1]))
+	print(len(s_arc[0][0]), len(s_rel[0][0]))
+
+example(sentences)
