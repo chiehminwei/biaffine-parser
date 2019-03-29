f448b138043fbc488f5c989b5bdfae9dacd16052
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-29 15:04:54 +0000

    debugging all UD

--- parser/utils/reader.py
+++ parser/utils/reader.py
@@ -81,7 +81,7 @@ class Corpus(object):
                     sentences.append(sentence)
                     start = i + 1
                 except:
-                    print(zip(*[l.split() for l in lines[start:i] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]]))
+                    print([l.split() for l in lines[start:i] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]])
                     assert 1 == 2
         corpus = cls(sentences)
 
