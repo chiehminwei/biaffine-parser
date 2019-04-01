5c7616a0091961bdb26347981d79b86b00ba7421
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-01 01:10:53 +0100

    distributed should now work

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
     decay_steps = 5000
 
     # [Run]
-    batch_size = 24 * 4
+    batch_size = 32
     epochs = 1000
     patience = 100
     gradient_accumulation_steps = 2
