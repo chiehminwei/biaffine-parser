be7a263379fc4ca6676cdb9743ca98e8630e96d9
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-01 02:01:43 +0100

    ah yea

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python create_datasets.py -c=gs://bert-chinese-mine/wut_wut_wut/
-python -m torch.distributed.launch --nproc_per_node=4 run.py train -c=gs://bert-chinese-mine/wut_wut_wut/
+python create_datasets.py -c=gs://bert-chinese-mine/wut_wut_wut/ && python -m torch.distributed.launch --nproc_per_node=4 run.py train -c=gs://bert-chinese-mine/wut_wut_wut/
