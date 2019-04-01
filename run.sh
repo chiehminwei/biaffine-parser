47a37b857ed2479d79873b0a86154897b35e6b52
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-01 01:15:58 +0100

    distributed should now work

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m torch.distributed.launch --nproc_per_node=4 run.py train
+python -m torch.distributed.launch --nproc_per_node=4 run.py train -c=gs://bert-chinese-mine/wut_wut_wut/
