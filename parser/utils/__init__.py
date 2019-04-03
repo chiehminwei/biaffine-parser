09f6961fd3409983e24a717ace00e9b7e02e512d
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-03 10:57:06 +0100

    e

--- parser/utils/__init__.py
+++ parser/utils/__init__.py
@@ -3,6 +3,7 @@
 from .dataset import TextDataset, collate_fn
 from .reader import Corpus
 from .vocab import Vocab
+import tokenization
 
 
-__all__ = ['Corpus', 'TextDataset', 'Vocab', 'collate_fn']
+__all__ = ['Corpus', 'TextDataset', 'Vocab', 'collate_fn', 'tokenization']
