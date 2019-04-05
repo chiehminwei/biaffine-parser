5958248bf637edc1e578ac30870e6fc399346c8b
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-05 03:28:14 +0100

    e

--- yeet.sh
+++ yeet.sh
@@ -4,6 +4,12 @@
 path="all";
 for conllx_file in ${path}/*.conllx; do
 	filename=$(echo $conllx_file | cut -d '/' -f 2 | cut -d '.' -f 1)
+	echo ' '
+	echo '***************************************'
 	echo $filename
+	echo '***************************************'
+	echo ' '
 	# bash CUDA_VISIBLE_DEVICES=2,3 python run.py train -c=gs:/dsfsdfs --ftrain=$conllx_file --ftrain_cache= --fdev_cache=
+	echo '***************************************'
+	echo ' '
 done
