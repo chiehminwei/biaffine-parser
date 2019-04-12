bd9ce25ddd119f05abe467e9533c7143b9f01a2a
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-13 04:49:48 +0800

    syntactic

--- example.py
+++ example.py
@@ -175,7 +175,7 @@ my_syntactic_embeddings = {
 for input_path, output_path in zip(corpus.values(), my_embeddings.values()):
 	print(input_path)
 	print(output_path)
-	write_hdf5(input_path, output_path, model=model)
+	write_hdf5(input_path, output_path, model=syntactic_model)
 
 # for input_path, output_path in zip(corpus.values(), my_syntactic_embeddings.values()):
 # 	write_hdf5(input_path, output_path, model=syntactic_model)
