1febc4e74f05401ef562c33f565a32188e275931
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-12 10:13:42 +0800

    oom

--- example.py
+++ example.py
@@ -55,11 +55,9 @@ def write_hdf5(input_path, output_path, model):
 			line = line.strip()
 			line = '[CLS] ' + line + ' [SEP]'
 			tokenized_text = tokenizer.wordpiece_tokenizer.tokenize(line)
-			indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-			token_start_mask = [1 for x in tokenized_text]	
-			print(indexed_tokens)
-			print(token_start_mask)		
-
+			indexed_tokens = torch.tensor(tokenizer.convert_tokens_to_ids(tokenized_text))
+			token_start_mask = torch.ByteTensor([1 for x in tokenized_text])
+			
 			dataset = TextDataset(([indexed_tokens], [token_start_mask], [token_start_mask]))
 			loader = DataLoader(dataset=dataset,
 								batch_size=BATCH_SIZE)
