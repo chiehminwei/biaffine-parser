76acccf77f24a2a1c093590a7faf3758c7ccb204
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-14 01:21:50 +0800

    avg embeddings

--- parser/model.py
+++ parser/model.py
@@ -249,7 +249,7 @@ class Model(object):
                 token_start_mask[torch.arange(len(token_start_mask)), lens] = 0
 
             if ignore_token_start_mask:
-                token_start_mask = torch.ones(token_start_mask.size(), dtype=torch.uint8)
+                token_start_mask = attention_mask
 
             embed = self.network.get_embeddings(words, attention_mask, return_all=return_all)
             
