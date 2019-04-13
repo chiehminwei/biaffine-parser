17a70ec43796054f8322c11d38dbcdb6baf8a1bf
Chiehmin Wei <cmw025@bucknell.edu>
2019-04-14 01:38:35 +0800

    avg embeddings

--- parser/model.py
+++ parser/model.py
@@ -296,15 +296,13 @@ class Model(object):
                             if tmp_len == 0:
                                 tmp_len = 1
                             sent_avg_embeddings.append(tmp/tmp_len)
-                        word_embed = np.array(word_embed)
-                        tmp = word_embed
+                        tmp = np.array(word_embed)
                         tmp_len = 1
                         if word_att_mask != 1:
                             break
                     else:
                         if tmp is not None:
-                            word_embed = np.array(word_embed)
-                            tmp += word_embed
+                            tmp += np.array(word_embed)
                             tmp_len += 1
                 all_embeddings.append(np.array(sent_avg_embeddings))
 
