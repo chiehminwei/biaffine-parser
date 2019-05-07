a5bd1456ca86c2066c199980bc69beff361fe254
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-07 20:46:31 +0100

    lstm fixed

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config(object):
     mlp_dropout = 0.33
 
     # [Optimizer]
-    # lr = 2e-3 # original
-    lr = 1e-5 # better for fine-tuning BERT and LM
+    lr = 2e-3 # original
+    # lr = 1e-5 # better for fine-tuning BERT and LM
 
     # [Run]
     batch_size = 2
