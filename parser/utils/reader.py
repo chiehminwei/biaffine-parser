6036d1a75c20d2272a88fe19da797f2dbf767d59
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-07 21:43:39 +0100

    wtf

--- parser/utils/reader.py
+++ parser/utils/reader.py
@@ -83,7 +83,7 @@ class Corpus(object):
             if line[0] == '#':
                 start += 1
             if len(line) <= 1:
-                sentence = Sentence(*zip(*[l.split('\t') for l in lines[start:i] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]]))
+                sentence = Sentence(*zip(*[l.split('\t') for l in lines[start:i+1] if "." not in l.split('\t')[0] and "-" not in l.split('\t')[0]]))
                 if len(sentence.ID) > 0:
                     sentences.append(sentence)
                 start = i + 1
