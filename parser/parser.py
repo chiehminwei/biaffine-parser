387e373dc7c61cd048a04f0f2a6162766ada0495
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-07 21:59:40 +0100

    wtf

--- parser/parser.py
+++ parser/parser.py
@@ -165,8 +165,8 @@ class BiaffineParser(nn.Module):
         s_rel = self.rel_attn(rel_d, rel_h).permute(0, 2, 3, 1)
 
         # set the scores that exceed the length of each sentence to -inf
-        # len_mask = length_to_mask(lens, max_len=input_ids.shape[-1], dtype=torch.uint8)
-        # s_arc.masked_fill_((1 - len_mask).unsqueeze(1), float('-inf'))
+        len_mask = length_to_mask(lens, max_len=input_ids.shape[-1], dtype=torch.uint8)
+        s_arc.masked_fill_((1 - len_mask).unsqueeze(1), float('-inf'))
 
         if masked_lm_labels is None:
             return s_arc, s_rel
