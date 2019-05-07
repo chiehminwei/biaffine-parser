df3d586607df62f7fbf7fae5db7255d09dc89896
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-07 21:57:33 +0100

    wtf

--- parser/utils/vocab.py
+++ parser/utils/vocab.py
@@ -157,7 +157,7 @@ class Vocab(object):
                     continue
                 
             # error checking for lengths
-            if kkk < 10:
+            if kkk < 3:
                 print(words)
                 print(self.tokenizer.convert_ids_to_tokens(sentence_token_ids))
             len_sentence_token_ids = len(sentence_token_ids)
