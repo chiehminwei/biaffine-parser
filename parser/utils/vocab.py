84306cb87bbd48fc046185feb7db625b281aa106
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-09 09:22:37 +0100

    fck

--- parser/utils/vocab.py
+++ parser/utils/vocab.py
@@ -146,7 +146,6 @@ class Vocab(object):
             del _
             for layer in range(8, 12):
                 layers.append(torch.stack([_ for i, _ in enumerate(bert_output[layer]) if token_starts[i] == 1]))
-            print(layers[0].shape)
             bert_embeddings = torch.sum(torch.stack(layers), dim=0)
             
             words_numerical.append(bert_embeddings)
