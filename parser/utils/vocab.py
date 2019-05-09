5d182bf83c8031d81fc7ad0c33b176090683868d
Chiehmin Wei <cmw025@bucknell.edu>
2019-05-09 10:04:26 +0100

    fck

--- parser/utils/vocab.py
+++ parser/utils/vocab.py
@@ -165,10 +165,10 @@ class Vocab(object):
                 print(words)
                 print(sum(token_starts))
                 print(layers[0].shape)
-                print(words_numerical[0].shape)
-                print(arcs_numerical[0].shape)
-                print(rels_numerical[0].shape)
-                print(tags_numerical[0].shape)
+                print(words_numerical[-1].shape)
+                print(arcs_numerical[-1].shape)
+                print(rels_numerical[-1].shape)
+                print(tags_numerical[-1].shape)
 
         if offending_set: 
             logging.warning('WARNING: The following non-symbol characters are unknown to BERT:')
